fa87ddb4e96419b0391255211dcf93fb9016fe44
shuvornoy <shuvornoy@gmaol.com>
2022-10-21 03:26:33 -0700

    all compet

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
 
-const categories = require('./Data/categories.json')
+const categories = require('./Data/categories.json');
+const news = require('./Data/news.json');
 
 app.use(cors())
 
@@ -15,6 +16,26 @@ app.get('/', (req, res) => {
 
   app.get('/news-categories', (req, res) =>{
     res.send(categories);
+  })
+
+  app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    if(id === '08'){
+      res.send(news)
+    }else{
+      const category_News = news.filter(n =>n.category_id === id)
+      res.send(category_News);
+    }
+  })
+
+  app.get('/news', (req, res) =>{
+    res.send(news);
+  })
+
+  app.get('/news/:id', (req, res) =>{
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
   })
   
   // POST method route
